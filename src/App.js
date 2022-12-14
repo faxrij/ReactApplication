276b0d35c3a77759f6e834a0d29e657ae6f9f372
fekyek <cfaxri@mail.ru>
2022-12-14 18:11:55 +0300

    Ready to deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetupsPage";
+import FavoritesPage from "./pages/FavoritesPage";
+import NewMeetupPage from "./pages/NewMeetupPage";
+import Layout from "./components/layout/Layout";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Layout>
+			<Routes>
+				<Route path="/" exact element={<AllMeetupsPage />}></Route>
+				<Route path="/new-meetup" element={<NewMeetupPage />}></Route>
+				<Route path="/favorites" element={<FavoritesPage />}></Route>
+			</Routes>
+		</Layout>
+	);
 }
 
 export default App;
