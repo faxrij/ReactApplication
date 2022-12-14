462931db9ba34bc21cfbbd38e97a6f3301c897f6
fekyek <cfaxri@mail.ru>
2022-12-14 19:08:00 +0300

    added favorites page

--- src/pages/FavoritesPage.js
+++ src/pages/FavoritesPage.js
@@ -1,5 +1,52 @@
+import { useState, useEffect } from "react";
+import MeetupList from "../components/meetups/MeetupList";
+
+
+
 function FavoritesPage(){
-    return <div>Favorites Page</div>
+
+    const [isLoading, setIsLoading] = useState(true);
+	const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+
+    useEffect(() => {
+		setIsLoading(true);
+		fetch(
+			"https://react-getting-started-ffdf1-default-rtdb.firebaseio.com/favorites.json"
+		)
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				const meetups = [];
+
+				for (const key in data) {
+					const meetup = {
+						id: key,
+						...data[key],
+					};
+					meetups.push(meetup);
+				}
+				setIsLoading(false);
+				setLoadedMeetups(meetups);
+			});
+	}, []);
+
+	if (isLoading) {
+		return (
+			<section>
+				<p>Loading...</p>
+			</section>
+		);
+	}
+
+
+    return (
+		<section>
+			<h1>Favorite Meetups</h1>
+			<MeetupList meetups={loadedMeetups} />
+		</section>
+	);
 }
 
 export default FavoritesPage;
